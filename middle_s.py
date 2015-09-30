8d92c814eb0c6d194c247daf853c14550d3f8d26
LeoMayuzumi <Leo@LeoMayuzumi.local>
2015-09-30 18:18:16 +0900

    大会用リクエスト仕様書に合致するように変更

--- middle_s.py
+++ middle_s.py
@@ -36,50 +36,46 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		self.send_response(200)
 		self.end_headers()
 		self.wfile.write("ok")
-		print "score"
-		print most_efficient["score"]
-		print form["score"].value
-		print "used_pieces"
-		print most_efficient["used_pieces"]
-		print form["used_pieces"].value
 
 		if most_efficient["score"] > int(form["score"].value):
-			requests.post(SERVER, data={"token": TOKEN,"answer": form["answer_string"].value})
-
 			most_efficient = {"problem_id":int(form["problem_id"].value),"answer_string":form["answer_string"].value,"score":int(form["score"].value),"time":int(form["time"].value),"version":form["version"].value,"used_pieces":int(form["used_pieces"].value)}
-			print most_efficient
 			strs = form["answer_string"].value
 			files = os.listdir(dirlists)
 			filelists = len(files)
 			filelists +=1
-			print filelists
 			f = open(dirlists+"/"+"answer"+str(filelists)+".txt","w")
 			f.writelines(strs)
 			print "answer"+str(filelists)+".txt"
-			print strs
 			f.close()
+			files = {'answer': ('answer.txt', open(dirlists+"/"+"answer"+str(filelists)+".txt", 'rb'))}
+			r = requests.post(SERVER, data={"token": TOKEN,},files = files)
+			print r.text
+
 		elif most_efficient["score"] == int(form["score"].value):
 			if most_efficient["used_pieces"] > int(form["used_pieces"].value):
-				requests.post(SERVER, data={"token": TOKEN,"answer": form["answer_string"].value})
-				most_efficient = {"problem_id":int(form["problem_id"].value),"answer_string":form["answer_string"].value,"score":int(form["score"].value),"time":int(form["time"].value),"version":form["version"].value,"used_pieces":int(form["used_pieces"].value)}
-				print most_efficient
+				most_efficient = {"problem_id":int(form["problem_id"].value),"answer_string":form["answer_string"].value,"score":int(form["score"].value),"time":int(form["time"].value),"version":form["version"].value,"used_pieces":int(form["used_pieces"].value)}				
 				strs = form["answer_string"].value
 				files = os.listdir(dirlists)
 				filelists = len(files)
 				filelists +=1
-				print filelists
 				f = open(dirlists+"/"+"answer"+str(filelists)+".txt","w")
 				f.writelines(strs)
 				print "answer"+str(filelists)+".txt"
-				print strs
 				f.close()
+				files = {'answer': ('answer.txt', open(dirlists+"/"+"answer"+str(filelists)+".txt", 'rb'))}
+				r = requests.post(SERVER, data={"token": TOKEN,},files = files)
+				print r.text
+
 if __name__ == "__main__":
 
 	path = "Dirs/"
+	if "Dirs" not in os.listdir("./"):
+		os.mkdir("Dirs")
+
 	dirs = os.listdir(path)
 	dirlists = len(dirs)
-	if dirlists == 1:
-		dirlists -=1
+	#if dirlists == 1:
+	#	dirlists -=1
 	dirlists +=1
 	dirlists = path+"answers"+str(dirlists)
 	os.mkdir(dirlists)
@@ -90,7 +86,7 @@ if __name__ == "__main__":
 	Handler = ServerHandler
 	httpd = SocketServer.TCPServer((HOST, PORT), Handler)
 
-	print "serving at port", PORT
+	print "serving at %s:%s"%(socket.gethostbyname(socket.gethostname()),PORT)
 	print "ready post to %s, at %s"%(SERVER,TOKEN)
 	try:
 		httpd.serve_forever()
